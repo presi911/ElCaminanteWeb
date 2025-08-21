9580a01bade8b5130e1ff3d6a07b00953150734c
David Clavijo <dfclavijo@gmail.com>
2025-08-20 20:18:17 -0500

    Solving render errors

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,19 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    path: '',
+    renderMode: RenderMode.Prerender, // Home: prerenderiza en build
+  },
+  {
+    path: 'crear-producto',
+    renderMode: RenderMode.Prerender, // Prerenderiza en build
+  },
+  {
+    path: 'productos',
+    renderMode: RenderMode.Prerender, // Prerenderiza en build
+  },
+  {
+    path: 'editar-producto/:id',
+    renderMode: RenderMode.Server, // Solo SSR, NO prerender
+  },
 ];
