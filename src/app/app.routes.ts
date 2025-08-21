86ba502eb7279502ecb0f06a800b03cbb886f232
David Clavijo <dfclavijo@gmail.com>
2025-08-20 19:53:54 -0500

    Render Default

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes } from '@angular/router';
 import { AdminDashboard } from '../admin-dashboard/admin-dashboard';
 import { ProductCreate } from '../products/products/components/product-create/product-create';
 import { ProductList } from '../products/products/components/product-list/product-list';
-import { ProductEdit } from '../products/products/components/product-edit/product-edit';
+import { ProductEdit } from '../products/products/components/product-edit/product-edit'; // Asegúrate de importar el componente
 
 export const routes: Routes = [
   { path: '', component: AdminDashboard },
   { path: 'crear-producto', component: ProductCreate },
   { path: 'productos', component: ProductList },
-  { path: 'editar-producto/:id', component: ProductEdit }, // <-- Nueva ruta
+  // @ts-expect-error
+  { path: 'editar-producto/:id', component: ProductEdit, renderMode: 'default' }, // Desactiva prerendering aquí
 ];
